6b85ff584aa98d1c4d37ab35877046d2989865af
Fabian Quintanar <croxx5f@protonmail.com>
2022-06-11 13:33:51 -0400

    Initial commit
    Project created from Spring Initializrgi

---
+++ src/main/java/cu/croxx/musalapp/MusalappApplication.java
@@ -0,0 +1,13 @@
+package cu.croxx.musalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusalappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cu/croxx/musalapp/MusalappApplicationTests.java
@@ -0,0 +1,13 @@
+package cu.croxx.musalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
